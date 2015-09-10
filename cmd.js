72202b65bd773364df53d0cfcba213169f2943dd
alexander-daniel <alex.vados@gmail.com>
2015-09-10 09:22:02 -0400

    rgba -> hex

--- cmd.js
+++ cmd.js
@@ -4,6 +4,8 @@
 
 var getPixels = require('get-pixels');
 var fs = require('fs');
+var rgb2hex = require('rgb2hex');
+
 var image = process.argv[2];
 var output = process.argv[3];
 
@@ -15,19 +17,20 @@ var ProgressBar = require('progress');
 getPixels(image, function(err, pixels) {
     if (err) return console.log('Bad pixels path');
     console.log('processing ' + image + ' -> SVG');
+
     var bar = new ProgressBar(':percent', { total: (pixels.data.buffer.byteLength / 4) });
     var shape = pixels.shape.slice();
     var width = shape[0];
     var height = shape[1];
-    
-    svgStream.write('<?xml version="1.0" encoding="utf-8" ?>'+
-                    '<svg baseProfile="full" ' + 
-                    'version="1.1" ' + 
-                    'height="'+height+'px" ' + 
-                    'width="'+width+'px" ' + 
+
+    svgStream.write('<?xml version="1.0" encoding="utf-8" ?>\n'+
+                    '<svg baseProfile="full" ' +
+                    'version="1.1" ' +
+                    'height="'+height+'px" ' +
+                    'width="'+width+'px" ' +
                     'xmlns="http://www.w3.org/2000/svg" ' +
                     'xmlns:ev="http://www.w3.org/2001/xml-events" ' +
-                    'xmlns:xlink="http://www.w3.org/1999/xlink"><defs />');
+                    'xmlns:xlink="http://www.w3.org/1999/xlink">\n');
 
     var c = 0;
     for (var i = 0; i < height; i += 1) {
@@ -37,16 +40,17 @@ getPixels(image, function(err, pixels) {
             var b = pixels.data.buffer[c+2];
             var a = pixels.data.buffer[c+3];
 
-            var rgbaStr = 'rgba('+r+','+g+','+b+','+a+')';
-            var rectStr = '<rect fill="'+ rgbaStr +'" height="1px" width="1px" x="' + j + '" y="'+ i +'" />';
+            var rgbStr = 'rgb('+r+','+g+','+b+')';
+            var hexObj = rgb2hex(rgbStr);
+            var rectStr = '<rect fill="'+ hexObj.hex +'" height="1px" width="1px" x="' + j + '" y="'+ i +'" />\n';
             svgStream.write(rectStr);
             bar.tick();
             c += 4;
         }
     }
-    
+
     svgStream.end('</svg>', function () {
         console.log('done!');
     });
-    
+
 });
